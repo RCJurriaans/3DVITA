5bc872f6b685dbf685ebc18149d0b2f906bc1e5e
RCJurriaans <robrechtjurriaans@gmail.com>
2013-03-31 15:06:32 +0200

    Added overview figure

--- Code/3dvita/3DVITA/main.cpp
+++ Code/3dvita/3DVITA/main.cpp
@@ -5,25 +5,41 @@ using namespace std;
 
 
 
-int main( int argc, char *argv[] )
-{
-	SoundManager Sound;
-
-	float location[3] = {0., 0., 2.};
-	float frequency;
-	float r[3] = {0., 0., 0.};
-
-	while(true){
-		frequency = rand() %479 +261;
-		r[1] = rand()%5 -2;
-		//r[2] = rand()%5 -2;
-		r[3] = rand()%5 -2;
-		cout << r[1] << r[2] << r[3] << endl;
-		AudioSource test = AudioSource(r, frequency);
-
-		test.play();
-		Sleep(150);
-	}
-
-	return 0;
-}
+//int main( int argc, char *argv[] )
+//{
+//	SoundManager Sound;
+//
+//	float location[3] = {0., 0., 1.};
+//	float frequency;
+//	float note;
+//	float notes[8] = {40, 44, 47, 49, 50, 49,  47, 44};
+//	//float notes[8] = {40, 50, 49, 50, 44, 47,  44, 40};
+//	float blues[10] = {0, 0, 5, 5, 0, 0, 7, 5, 0, 0};
+//	float time;
+//
+//	int i=0;
+//	int blue = 0;
+//	while(true){
+//		location[0] = rand()%5-2;
+//		location[2] = rand()%5-2;
+//		note = notes[i]+blues[blue];
+//		cout << "Note: " << note << endl;
+//		frequency = 440.f * pow(2, (( note-49) /12));
+//		
+//		AudioSource test = AudioSource(location, frequency);
+//
+//		test.play();
+//		Sleep(150);
+//		
+//
+//		i++;
+//		if(i>7){
+//			blue++;
+//			i=0;
+//		}
+//		if(blue>9)
+//			break;
+//	}
+//
+//	return 0;
+//}
