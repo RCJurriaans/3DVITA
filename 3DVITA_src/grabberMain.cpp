1626e3eda50d597b300b161ab31e834f4129281b
RCJurriaans <robrechtjurriaans@gmail.com>
2013-05-03 14:56:10 +0200

    Removed separate programs until prototype is done. Added OpenCV to project and started on the basic segmentation for the prototype (see wiki)

--- 3DVITA_src/grabberMain.cpp
+++ 3DVITA_src/grabberMain.cpp
@@ -1,44 +1,64 @@
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 #include <pcl/io/openni_grabber.h>
-#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/common/time.h>
 
- class SimpleOpenNIViewer
- {
-   public:
-     SimpleOpenNIViewer () : viewer ("PCL OpenNI Viewer") {}
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-     void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr &cloud)
-     {
-       if (!viewer.wasStopped())
-         viewer.showCloud (cloud);
-     }
+class SimpleOpenNIViewer
+{
+public:
+	cv::Mat frameRGB;
 
-     void run ()
-     {
-       pcl::Grabber* interface = new pcl::OpenNIGrabber();
+	SimpleOpenNIViewer(){
+		cv::namedWindow("input", CV_WINDOW_AUTOSIZE);
+		frameRGB = cv::Mat(480, 640, CV_8UC3);
+	}
 
-	   
+	void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr &cloud)
+	{
 
-       boost::function<void (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr&)> f =
-         boost::bind (&SimpleOpenNIViewer::cloud_cb_, this, _1);
+	}
 
-       interface->registerCallback (f);
+	void rgbd_cb_ ( const boost::shared_ptr<openni_wrapper::Image>      &image_in, 
+		const boost::shared_ptr<openni_wrapper::DepthImage> &depth_in, 
+		float constant_in) 
+	{
 
-       interface->start ();
+		image_in->fillRGB(frameRGB.cols,frameRGB.rows,frameRGB.data,frameRGB.step);
 
-       while (!viewer.wasStopped())
-       {
-         boost::this_thread::sleep (boost::posix_time::seconds (1));
-       }
+	}
 
-       interface->stop ();
-     }
+	void run ()
+	{
+		pcl::Grabber* interface = new pcl::OpenNIGrabber();
 
-     pcl::visualization::CloudViewer viewer;
- };
+		boost::function<void (const boost::shared_ptr<openni_wrapper::Image>&, const boost::shared_ptr<openni_wrapper::DepthImage>&, float constant)> rgbd =\
+			boost::bind (&SimpleOpenNIViewer::rgbd_cb_, this, _1,_2,_3);
 
- int main ()
- {
-   SimpleOpenNIViewer v;
-   v.run ();
-   return 0;
- }
+		boost::signals2::connection c = interface->registerCallback (rgbd);
+
+		interface->start();
+
+		int k = -1;
+		while (k==-1)
+		{
+			//boost::this_thread::sleep (boost::posix_time::seconds (1));
+			cv::imshow("input", frameRGB );
+			k = cv::waitKey(33);
+		}
+
+		interface->stop ();
+	}
+
+
+};
+
+
+int main ()
+{
+	SimpleOpenNIViewer v;
+	v.run ();
+	return (0);
+}
